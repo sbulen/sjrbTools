ac94a7474d3606b3c2de92dd105d8112e80905e8
sbulen <bulens@pacbell.net>
2021-12-22 21:08:30 -0800

    Convert smf_quote_link_fixer.php

--- README.md
+++ README.md
@@ -11,8 +11,7 @@ These tools are inquiry only & are used to perform diagnostics.
 
 * ***smf_db_compare.php*** - This tool compares your database information to a vanilla SMF database, highlighting any changes. It works for all SMF1.x & 2.x forums.
 * ***smf_img_proxy_diag.php*** - This tool dumps key info about the environment & a particular user-specified image.  Used to understand Image Proxy issues.  It works for SMF2.0 & 2.1. 
-* ***smf_settings_diag.php*** - This tool dumps key settings.php entries and dumps the entire SMF settings table.  It works for SMF2.0 & 2.1.  ***NOTE -*** Some mods place sensitive information like passwords & hashes in the SMF settings table.  You may not want to share the output of this diagnostic publicly.  
-* ***SMF_Topic_Link_Diag.php*** - This tool dumps any quote link that appears to have issues, such as a missing originating message or an incorrect topic caused by splits/merges.  It works for SMF2.0 & 2.1.
+* ***smf_settings_diag.php*** - This tool dumps key settings.php entries and dumps the entire SMF settings table.  It works for SMF2.0 & 2.1.  ***NOTE -*** Some mods place sensitive information like passwords & hashes in the SMF settings table.  You may not want to share the output of this diagnostic publicly.
 * ***smf_utf8_diag.php*** - This tool dumps a bunch of helpful information on the state of the character set & collation of your SMF database.  It works for SMF2.0 & 2.1.
 * ***smf_hex_dump.php*** - This tool dumps occurances of a specific column in a specific record in hex.  Useful for debugging UTF8 data issues.  It is UTF8 safe - it will not chop a multi-byte character in half.  It works for SMF2.0 & 2.1. 
 
@@ -27,6 +26,7 @@ These tools are inquiry only & are used to perform diagnostics.
 
 * ***smf_innodb_converter.php*** - This tool converts your SMF forum's MySQL database engine to InnoDB.  SMF 2.0 & 2.1.  
 * ***SMF_URLs_Paths.php*** - This tool converts all URLs and Paths throughout your SMF forum's database from one value to another.  SMF 2.0 & 2.1.  This tool updates all URLs and Paths found throughout the settings table, the themes table, the messages table, personal messages and members' signatures.  I use this to quickly clone working test environments, so they do not link to each other and I do not find myself working within the wrong environment after clicking on a link...  (Its original name was really_really_really_repair_settings.php, but I felt that was too long...)
+* ***smf_quote_link_fixer.php*** - Checks & fixes all quote links.  May be used to correct any quote link with issues, such as a deleted originating message or an incorrect topic caused by splits/merges. It works for SMF2.0 & 2.1.
 * ***smf_fix_log_actions.php*** - Checks & fixes all string lengths in log_actions. UTF8 conversions can break string lengths in serialized strings.  SMF2.0.
 * ***smf_remove_old_bbc.php*** - Removes old BBC.  Provided a regex, substitutes the first captured group for the entire match.  Helpful when you no longer use old mods that added BBC to posts at some point.  SMF2.0 & 2.1.
 
---
+++ smf_quote_link_fixer.php
@@ -0,0 +1,768 @@
+<?php
+/**
+ *
+ * An SMF utility to identify and FIX quote links that may be damaged.
+ * This might happen in older versions of SMF after topics have been split or merged after messages were quoted.
+ *
+ * ***** SMF 2.0 & 2.1 *****
+ * ***** MySQL & Postgresql *****
+ *
+ * Usage guidelines:
+ * (1) Use at your own risk.
+ * (2) ALWAYS run in your test environment first.
+ * (3) ALWAYS backup your system first - expect the unexpected.
+ * (4) Execute it from your browser.
+ * (5) Run in preview mode first.
+ * (6) If things look good, click the Proceed button.
+ * (7) Delete it when you're done.
+ *     by sbulen
+ *
+ */
+
+$site_title = 'SMF Quote Link Fixer';
+$db_needed = true;
+$ui = new SimpleSmfUI($site_title, $db_needed);
+
+$ui->addChunk('Settings', function() use ($ui)
+{
+	global $smcFunc, $db_connection, $db_type, $sourcedir;   // Must remain globals
+
+	// First some settings file stuff...
+	$dumpvars = array('mbname', 'boardurl', 'db_server', 'db_name', 'db_prefix', 'language', 'db_type', 'db_character_set', 'db_mb4');
+
+	$settings = array();
+	$settings[0] = array('Variable','Value');
+	foreach($dumpvars AS $var)
+	{
+
+		if (!isset($ui->getSettingsFile()[$var]))
+			$value = '<strong>NOT SET</strong>';
+		elseif (is_null($ui->getSettingsFile()[$var]))
+			$value = '<em>null</em>';
+		elseif ($ui->getSettingsFile()[$var] === false)
+			$value = '<em>false</em>';
+		elseif ($ui->getSettingsFile()[$var] === true)
+			$value = '<em>true</em>';
+		else
+			$value = $ui->getSettingsFile()[$var];
+
+		$settings[] = array($var, $value);
+	}
+	$ui->dumpTable($settings);
+
+});
+
+$ui->addChunk('Preview or Proceed?', function() use ($ui)
+{
+	global $smcFunc, $db_type, $db_connection, $db_prefix, $db_name;
+
+	echo '<form>';
+	echo '<br><input type="submit" class="button" formmethod="post" name="preview" value="Preview?"><br>';
+	echo '<br><input type="submit" class="button" formmethod="post" name="proceed" value="Proceed?"><br>';
+	echo '</form>';
+
+});
+
+$ui->addChunk('Results', function() use ($ui)
+{
+	global $smcFunc, $db_connection, $db_prefix;
+
+	// Gotta hit a button...
+	if (empty($_SESSION['preview']) && empty($_SESSION['proceed']))
+		return;
+
+	if (!empty($_SESSION['proceed']))
+		echo '<br><strong>Executing:</strong><br>';
+	else
+		echo '<br><strong>Preview:</strong><br>';
+
+	// Look at all the messages...
+	$result = $smcFunc['db_query']('', '
+		SELECT id_msg, body FROM {db_prefix}messages;',
+		array(
+		)
+	);
+
+	// Setup table for display
+	$ui->updates = array();
+	$ui->updates[0] = array('Message ID', 'Broken Link', 'Note', 'New Link');
+
+	// Check each...
+	while ($message = $smcFunc['db_fetch_assoc']($result))
+		checkMessage($message, $ui);
+	
+	$smcFunc['db_free_result']($result);
+
+	if (count($ui->updates) > 1)
+		$ui->dumpTable($ui->updates);
+	else
+		echo '<br>No changes needed!<br>';
+});
+
+//*** Check ONE message
+function checkMessage($message, $ui)
+{
+	global $smcFunc, $db_type, $db_connection, $db_prefix, $db_name;
+
+	// Format of the old link type
+	// Matches [0] = whole match; [1] = author; [2] = topic; [3] = message [4] = date
+	$pattern = '/\[quote\sauthor=([^\s]*?)\slink=topic=(\d{1,10})\.msg\d{1,10}#msg(\d{1,10})\sdate=(\d{1,15})\]/i';
+	
+	// Allow for multiple quotes in the same message
+	preg_match_all($pattern, $message['body'], $matches);
+	
+	if (empty($matches[0]))
+		return;
+	
+	$updateneeded = false;
+	
+	foreach($matches[3] as $ix => $msg)
+	{
+		// Pluck the message ID for the link from the match string
+		$testmsgid = (int) $msg;
+	
+		// Get the message referenced in the link
+		$result = $smcFunc['db_query']('', '
+			SELECT id_topic FROM {db_prefix}messages
+			 WHERE id_msg = {int:msgid};',
+			array(
+				'msgid' => $testmsgid,
+			)
+		);
+		$referenced = $smcFunc['db_fetch_assoc']($result);
+		$smcFunc['db_free_result']($result);
+
+		// If you found the message...
+		$newlink = '';
+		if 	(!empty($referenced))
+		{
+			// And the topics don't match...
+			if ($matches[2][$ix] != $referenced['id_topic'])
+			{
+				$newlink = '[quote author= ' . $matches[1][$ix] . ' link=topic=' . $referenced['id_topic'] . '.msg' . $matches[3][$ix] . '#msg' . $matches[3][$ix] . ' date=' . $matches[4][$ix] . ']';
+				$ui->updates[] = array($message['id_msg'], $matches[0][$ix], 'New topic: ' . $referenced['id_topic'], $newlink);
+			}
+		}
+		else
+		{
+			// Or maybe you didn't find the message...
+			$newlink = '[quote author= ' . $matches[1][$ix] . ']';
+			$ui->updates[] = array($message['id_msg'], $matches[0][$ix], 'Message deleted', $newlink);
+		}
+
+		// Update the message body...
+		if (!empty($newlink))
+		{
+			$message['body'] = str_replace($matches[0][$ix], $newlink, $message['body']);
+			$updateneeded = true;
+		}
+	}
+
+	// Update DB with new message body
+	if ($updateneeded && !empty($_SESSION['proceed']))
+	{
+		$result = $smcFunc['db_query']('', '
+			UPDATE {db_prefix}messages
+			   SET body = {string:newbody}
+			 WHERE id_msg = {int:msgid};',
+			array(
+				'newbody' => $message['body'],
+				'msgid' => $message['id_msg'],
+			)
+		);
+	}
+}
+
+$ui->go();
+
+/**
+ * SimpleSmfUI
+ *
+ * A simple basic abstracted UI for utilities.
+ *
+ * Copyright 2021 Shawn Bulen
+ *
+ * This file is part of the sjrbTools library.
+ *
+ * SimpleSmfUI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimpleSmfUI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimpleSmfUI.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+class SimpleSmfUI
+{
+	/*
+	 * Properties
+	 */
+	protected $site_title = 'Simple UI';
+	protected $max_width = 1200;
+	protected $db_needed;
+	protected $txt = array(
+		'err_no_title' => 'Site title is required and must be a string!',
+		'err_width' => 'Funky width specified!',
+		'err_no_settings' => 'Could not find Settings.php!  Place this file in the same folder as Settings.php.',
+		'err_no_db' => 'Could not establish connection with the database!',
+		'err_no_chunk_title' => 'Invalid chunk title!',
+		'err_no_chunk_func' => 'Invalid chunk function!',
+		'errors' => 'Errors',
+	);
+	protected $chunks = array();
+	protected $errors = array();
+
+	/*
+	 * SMF Properties
+	 */
+	protected $settings_file;
+
+	/**
+	 * Constructor
+	 *
+	 * Builds a SimpleSmfUI object
+	 *
+	 * @param string title
+	 * @param bool db_needed
+	 * @return void
+	 */
+	function __construct($title, $db_needed = null, $max_width = 800)
+	{
+		// Might as well try...
+		@set_time_limit(6000);
+		@ini_set('memory_limit', '512M');
+
+		// Title...
+		if (is_string($title))
+			$this->site_title = $title;
+		else
+			$this->addError('err_no_title');
+
+		// db_needed...
+		if (empty($db_needed))
+			$this->db_needed = false;
+		else
+			$this->db_needed = true;
+
+		// Width...
+		if (is_numeric($max_width))
+			$this->max_width = $max_width;
+		else
+			$this->addError('err_width');
+
+		// Error handler
+		// Note that php error suppression - @ - still calls the error handler.  It will return 0 as it does so (pre php8).
+		// Note error handling in php8+ no longer fails silently on many errors, but error_reporting()
+		// will return 4437 (E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR | E_PARSE)
+		// as it does so.
+		set_error_handler(
+			function($errno, $errstr, $errfile, $errline)
+			{
+				if ((error_reporting() != 0) && (error_reporting() != (E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR | E_PARSE)))
+					$this->addError($errstr . ' (' . $errno . ')');
+				// Always try & report errors gracefully...
+				return true;
+			}
+		);
+
+		// DB...
+		define('SMF', 1);
+		define('SMF_VERSION', '2.x');
+		define('SMF_FULL_VERSION', 'SMF ' . SMF_VERSION);
+		define('SMF_SOFTWARE_YEAR', '2021');
+
+		define('POSTGRE_TITLE', 'PostgreSQL');
+		define('MYSQL_TITLE', 'MySQL');
+		define('SMF_USER_AGENT', 'Mozilla/5.0 (' . php_uname('s') . ' ' . php_uname('m') . ') AppleWebKit/605.1.15 (KHTML, like Gecko)  SMF/' . strtr(SMF_VERSION, ' ', '.'));
+
+		// These must remain globals when calling SMF funcs...
+		global $smcFunc, $db_connection, $db_prefix, $db_name, $db_type, $sourcedir, $cachedir;
+		$smcFunc = array();
+		$this->settings_file = array();
+
+		if ($this->db_needed)
+		{
+			// Load & save off settings file contents
+			if (file_exists('Settings.php'))
+			{
+				include_once('Settings.php');
+				$dumpvars = array('mbname', 'db_server', 'db_name', 'db_prefix', 'db_type', 'db_character_set', 'db_mb4', 'language',
+					'boardurl', 'boarddir', 'sourcedir', 'packagesdir', 'tasksdir', 'cachedir',
+					'maintenance', 'mtitle', 'mmessage',
+					'cookiename', 'db_persist', 'db_error_send',
+					'cache_accelerator', 'cache_enable', 'cache_memcached',
+					'image_proxy_enabled', 'image_proxy_secret', 'image_proxy_maxsize');
+
+				foreach($dumpvars as $setting)
+					$this->settings_file[$setting] = (isset(${$setting}) ? ${$setting} : '<strong>NOT SET</strong>');
+			}
+			else
+				$this->addError('err_no_settings');
+
+			if (!empty($sourcedir))
+			{
+				// Get the database going!
+				if (empty($db_type) || $db_type == 'mysqli')
+					$db_type = 'mysql';
+
+				// Make the connection...
+				require_once($sourcedir . '/Subs-Db-' . $db_type . '.php');
+				$db_connection = smf_db_initiate($db_server, $db_name, $db_user, $db_passwd, $db_prefix);
+
+				if (empty($db_connection))
+					$this->addError('err_no_db');
+			}
+		}
+	}
+
+	/**
+	 * Render chunk
+	 *
+	 * Display one portion of the form
+	 *
+	 * @return void
+	 */
+	protected function doChunk($ix, $chunk)
+	{
+		echo '<section>';	// sections needed to narrow scope of expand/collapse action
+		echo '<input type="checkbox" name="collapse" checked id="chunk' . $ix . '">
+			<div class="chunkhdr">
+				<label for="chunk' . $ix . '">' . $chunk['title'] . '</label>
+			</div>
+			<div class="content_nopad">
+			<div class="content">';
+
+		$chunk['function']();
+
+		echo '</div></div>';
+		echo '</section>';
+	}
+
+	/**
+	 * Display errors
+	 *
+	 * Display errors in current display area
+	 *
+	 * @return void
+	 */
+	protected function renderErrors()
+	{
+		echo '<div id="errhdr">' . $this->txt['errors'] . '</div>
+			<div class="content_nopad">
+			<div class="content">';
+
+		foreach ($this->errors AS $error)
+			echo $error . '<br>';
+
+		echo '</div></div>';
+	}
+
+	/**
+	 * Render header
+	 *
+	 * Spits out the head, title, style & starts the body
+	 *
+	 * @return void
+	 */
+	protected function renderHeader()
+	{
+		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+		<html>
+		<head>
+		<meta charset="utf-8">
+		<title>' . $this->site_title . '</title>
+		<style type="text/css">
+			body
+			{
+				padding: 12px 0px 0px 0px;
+				font-family: "Roboto", sans-serif;
+				background-color: rgb(242,242,242);
+				max-width: ' . $this->max_width . 'px;
+				margin: 0 auto;
+				box-shadow: 2px 2px 2px 2px rgb(220,220,220);
+				border-bottom: 1px solid rgb(170,170,170);
+			}
+
+			#header
+			{
+				padding: 4px 4% 4px 4%;
+				background-color: rgb(54,126,189);
+				font-family: "Roboto", sans-serif;
+				font-size: x-large;
+				height: 30px;
+				vertical-align: middle;
+				text-align:center;
+				border-bottom: 1px solid rgb(58,123,155);
+			}
+
+			.content_nopad
+			{
+				background: linear-gradient(180deg, rgb(190,190,190) 0%, rgb(252,252,252) 5px);
+			}
+
+			.content
+			{
+				padding: 10px 10px 10px 10px;
+				font-size: small;
+			}
+
+			#nametag
+			{
+				font-family: "Roboto", sans-serif;
+				font-size: smaller;
+				color: rgb(122,132,134);
+				background-color: rgb(252,252,252);
+				text-align: right;
+				display: inline-block;
+				width: 50%;
+			}
+
+			#warningtag
+			{
+				font-family: "Roboto", sans-serif;
+				font-size: smaller;
+				color: rgb(122,132,134);
+				background-color: rgb(252,252,252);
+				text-align: left;
+				display: inline-block;
+				width: 50%;
+			}
+
+			.chunkhdr
+			{
+				padding: 4px 4% 4px 4%;
+				background-color: rgb(0,153,204);
+				font-family: "Roboto", sans-serif;
+				font-size: large;
+				height: 25px;
+				vertical-align: middle;
+				text-align:center;
+				border-bottom: 1px solid rgb(89,154,187);
+			}
+
+			.chunkhdr label
+			{
+				display: inline-block;
+				width: 100%;
+			}
+
+			.chunkhdr:hover,
+			.chunkhdr:focus
+			{
+				background: rgb(76,175,208);
+			}
+
+			input[name="collapse"]:not(:checked) ~ .content_nopad
+			{
+				display: none;
+			}
+
+			input[name="collapse"]
+			{
+				display: none;
+			}
+
+			.chunkhdr label:before {
+				content: "▲";
+				display: inline-block;
+				float: left;
+				vertical-align: middle;
+				color: rgb(54,126,189);
+			}
+
+			input[name="collapse"]:not(:checked) ~ .chunkhdr label:before
+			{
+				transform: rotate(180deg);
+				transform-origin: center;
+			}
+
+			#errhdr
+			{
+				padding: 4px 4% 4px 4%;
+				background-color: rgb(230,130,130);
+				font-family: "Roboto", sans-serif;
+				font-size: large;
+				height: 25px;
+				vertical-align: middle;
+				text-align:center;
+			}
+
+			input[type=text], select
+			{
+				width: 85%;
+				height: 25px;
+				margin: 4px 0px;
+				display: inline-block;
+				border: 1px solid #ccc;
+				border-radius: 4px;
+				box-sizing: border-box;
+			}
+
+			.button
+			{
+				padding: 1px 1px;
+				text-align: center;
+				width: 100px;
+				box-shadow: 1px 1px 1px 1px rgb(210,210,210);
+			}
+
+			.sui_table
+			{
+				display: table;
+				table-layout:fixed;
+				border-collapse: collapse;
+				border-spacing: 0;
+				padding: 0px;
+				border: 1px solid #ccc;
+				text-align: left;
+				vertical-align: top;
+				white-space: normal;
+				word-break: break-word;
+				font-weight: normal;
+				font-variant: normal;
+				color: inherit;
+			}
+
+			.sui_row
+			{
+				display: table-row;
+			}
+
+			.sui_row_red
+			{
+				display: table-row;
+				background-color: rgb(230,130,130);
+			}
+
+			.sui_row_green
+			{
+				display: table-row;
+				background-color: #42ddcf;
+			}
+
+			.sui_row_header
+			{
+				display: table-header-group;
+				font-weight: bold;
+				color: rgb(46,96,140);
+			}
+
+			.sui_cell
+			{
+				border: 1px solid #ccc;
+				display: table-cell;
+				padding: 2px;
+				min-width: 80px;
+				max-width: 750px;
+			}
+
+			.sui_cell_yellow
+			{
+				border: 1px solid #ccc;
+				display: table-cell;
+				padding: 2px;
+				min-width: 80px;
+				max-width: 750px;
+				background-color: #f7f793;
+			}
+		</style>
+		</head>
+		<body>
+		<div id="header">' . $this->site_title . '
+		</div>';
+	}
+
+	/**
+	 * Render header
+	 *
+	 * Closes out the body & html tags
+	 *
+	 * @return void
+	 */
+	protected function renderFooter()
+	{
+		// Close out body & html tags
+		echo '<div id="warningtag">Remove when not in use</div>';
+		echo '<div id="nametag">sbulen/sjrbTools</div>';
+		echo '</body>
+		</html>';
+	}
+
+	/**
+	 * Cleanse text
+	 *
+	 * Some basic hygiene for user-entered input
+	 *
+	 * @param string input
+	 * @param bool gtlt - whether to leave > and < alone (e.g., for queries)
+	 * @return string cleansed
+	 */
+	public function cleanseText($input, $gtlt = false)
+	{
+		$input = trim($input);
+		$input = htmlspecialchars($input);
+		if ($gtlt)
+		{
+			$input = str_replace('&gt;', '>', $input);
+			$input = str_replace('&amp;gt;', '>', $input);
+			$input = str_replace('&lt;', '<', $input);
+			$input = str_replace('&amp;lt;', '<', $input);
+		}
+		return $input;
+	}
+
+	/**
+	 * Dump table
+	 *
+	 * Render a simple 2-d array in table form
+	 *
+	 * @param array passed_array
+	 * @return void
+	 */
+	public function dumpTable($passed_array)
+	{
+		static $special_cells = array('<strong>NOT SET</strong>', '<em>null</em>', '<em>true</em>', '<em>false</em>');
+
+		$header = true;
+		echo '<br><div class="sui_table">';
+		foreach($passed_array as $row)
+		{
+			// Some cleansing...
+			foreach ($row AS $ix => $cell)
+			{
+				// Treat NOT SET, null, true, & false special...
+				if (in_array($cell, $special_cells))
+					$row[$ix] = $cell;
+				else
+				{
+					$row[$ix] = htmlspecialchars($cell);
+					// Undo any line breaks you just broke...
+					$row[$ix] = str_replace('&lt;br&gt;', '<br>', $row[$ix]); 
+					$row[$ix] = str_replace('&lt;br /&gt;', '<br>', $row[$ix]); 
+				}
+			}
+			if ($header)
+				echo '<div class="sui_row_header">';
+			else
+				echo '<div class="sui_row">';
+			echo '<div class="sui_cell">';
+			echo implode('</div><div class="sui_cell">', $row);
+			echo '</div></div>';
+			$header = false;
+		}
+		echo '</div><br>';
+	}
+
+	/**
+	 * Add Chunk
+	 *
+	 * Adds an entry to the internal chunk array.
+	 * Each chunk will display a header, do some logic, & display some content.
+	 * If errors are encountered, ideally they should be added to the errors display and displayed at the end.
+	 *
+	 * @param string title - title to display above this chunk
+	 * @param function logic - what to execute, passed as an anonymous function
+	 * @return void
+	 */
+	public function addChunk($title, $func)
+	{
+		if (!is_string($title))
+		{
+			$title = '';
+			$this->addError('err_no_chunk_title');
+		}
+
+		if (!is_callable($func))
+		{
+			$func = function() {};
+			$this->addError('err_no_chunk_func');
+		}
+
+		$this->chunks[] = array('title' => $title, 'function' => $func);
+	}
+
+	/**
+	 * Get Settings File contents
+	 *
+	 * @return array
+	 */
+	public function getSettingsFile()
+	{
+		return $this->settings_file;
+	}
+
+	/**
+	 * Add Error
+	 *
+	 * Add error to internal log
+	 *
+	 * @param string key - is key to $txt array
+	 * @param string more - is additional info to be added to output string if needed
+	 * @return void
+	 */
+	public function addError($key, $more = '')
+	{
+		if (!is_string($key))
+			$key = '';
+
+		if (!is_string($more))
+			$more = '';
+
+		if (!empty($this->txt[$key]))
+			$key = $this->txt[$key];
+
+		$this->errors[] = $key . ' ' . $more;
+	}
+
+	/**
+	 * Go
+	 *
+	 * Got everything, now do it...
+	 *
+	 * @return void
+	 */
+	public function go()
+	{
+		global $db_connection;
+
+		// Responding to a POST? Cleanse info, put in session and redirect
+		session_start();
+		if ($_POST)
+		{
+			$_SESSION = array();
+			foreach($_POST as $var => $val)
+				$_SESSION[$this->cleanseText($var)] = $this->cleanseText($val);
+			
+			// Redirect to this page
+			header("Location: {$_SERVER['REQUEST_URI']}", true, 302);
+			exit();
+		}
+
+		// OK, display stuff...
+		$this->renderHeader();
+
+		// Execute the chunks...
+		// Note if db_needed & no connection, do not process chunks, just display the errors
+		if (!$this->db_needed || ($this->db_needed && !empty($db_connection)))
+		{
+			foreach($this->chunks AS $ix => $chunk)
+				$this->doChunk($ix, $chunk);
+		}
+
+		// Display any errors...
+		if (!empty($this->errors))
+			$this->renderErrors();
+
+		$this->renderFooter();
+
+		// Ensure refreshes actually refresh!
+		$_SESSION = array();
+	}
+}
